a19cd9738b37ad611a780ce04563558d0e6609f2
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2018-04-26 13:52:35 -0400

    Add a simple app

---
+++ src/main/java/com/jugglinhats/pipelinepoc/PipelinepocApplication.java
@@ -0,0 +1,12 @@
+package com.jugglinhats.pipelinepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinepocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelinepocApplication.class, args);
+    }
+}
