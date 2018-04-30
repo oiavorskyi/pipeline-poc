4b93376cd6550d096aa8502117f92b2af0d9d26c
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2018-04-30 17:07:20 -0400

    Make compilation fail

--- src/main/java/com/jugglinhats/pipelinepoc/PipelinepocApplication.java
+++ src/main/java/com/jugglinhats/pipelinepoc/PipelinepocApplication.java
@@ -3,7 +3,7 @@ package com.jugglinhats.pipelinepoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+SpringBootApplication
 public class PipelinepocApplication {
 
     public static void main(String[] args) {
